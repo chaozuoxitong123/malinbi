52643db868457bde92986db37c56cb3f844d0ea6
老马 <3314991589@qq.com>
2023-09-20 13:26:39 +0800

    first

---
+++ malinbi-frontend/src/pages/User/Register/index.tsx
@@ -0,0 +1,176 @@
+import Footer from '@/components/Footer';
+import { listChartByPageUsingPOST } from '@/services/yubi/chartController';
+import {getLoginUserUsingGET, userLoginUsingPOST, userRegisterUsingPOST} from '@/services/yubi/userController';
+import { Link } from '@@/exports';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginForm, ProFormText } from '@ant-design/pro-components';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { Helmet, history, useModel } from '@umijs/max';
+import { message, Tabs } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { flushSync } from 'react-dom';
+import Settings from '../../../../config/defaultSettings';
+
+const Register: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const { initialState, setInitialState } = useModel('@@initialState');
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    };
+  });
+
+  useEffect(() => {
+    listChartByPageUsingPOST({}).then((res) => {
+      console.error('res', res);
+    });
+  });
+
+  /**
+   * 登陆成功后，获取用户登录信息
+   */
+  const fetchUserInfo = async () => {
+    const userInfo = await getLoginUserUsingGET();
+    if (userInfo) {
+      flushSync(() => {
+        setInitialState((s) => ({
+          ...s,
+          currentUser: userInfo,
+        }));
+      });
+    }
+  };
+
+  const handleSubmit = async (values: API.RegisterRequest) => {
+    //校验
+
+    try {
+      // 注册
+      const res = await userRegisterUsingPOST(values);
+      if (res.code === 0) {
+        const defaultLoginSuccessMessage = '注册成功！';
+        message.success(defaultLoginSuccessMessage);
+        await fetchUserInfo();
+        const urlParams = new URL(window.location.href).searchParams;
+        history.push(urlParams.get('redirect') || '/user/login');
+        return;
+      } else {
+        message.error(res.message);
+      }
+    } catch (error) {
+      const defaultLoginFailureMessage = '注册失败，请重试！';
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  return (
+    <div className={containerClassName}>
+      <Helmet>
+        <title>
+          {'注册'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          submitter={{
+            searchConfig:{
+              submitText: '注册'
+            }
+          }}
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" src="/logo.svg" />}
+          title="传奇智能 BI"
+          subTitle={
+            <a href="https://cnblogs.com/malinyan" target="_blank">
+              个人博客
+            </a>
+          }
+          onFinish={async (values) => {
+            await handleSubmit(values as API.RegisterRequest);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '账户注册',
+              },
+            ]}
+          />
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={'请输入用户名'}
+                rules={[
+                  {
+                    required: true,
+                    message: '用户名是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'请再次输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                ]}
+              />
+            </>
+          )}
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+            <Link to="/user/login">登录</Link>
+          </div>
+        </LoginForm>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Register;
